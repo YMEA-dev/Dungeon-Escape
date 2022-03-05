3ac8d20573f32febb973f180075d4a7873c0927c
YMEA-Kirigoulou <ymeaproject@gmail.com>
2022-03-05 19:57:47 +0100

    The enemy patrols now patrols across the map and follows the player when in range. Added MonsterType enum

---
+++ Assets/Scripts/IA/EnemyType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyType : MonoBehaviour
+{
+    public enum MonsterType
+    {
+        Wizard,
+        Skeleton,
+        Slime,
+        RollingStone,
+        Minotaur
+    }
+}
