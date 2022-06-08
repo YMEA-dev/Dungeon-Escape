7907b90015ebde995d130d950f23a0cbfdbc9f84
Mathias Dart <mathias.dart@epita.fr>
2022-06-08 22:18:54 +0200

    door sound

--- Assets/door/KeyGate.cs
+++ Assets/door/KeyGate.cs
@@ -4,10 +4,12 @@ namespace door
 {
     public class KeyGate : MonoBehaviour
     {
+        [SerializeField] private AudioClip clip;
         private void OnTriggerEnter(Collider collider)
         {
             if (collider.CompareTag("Player") && GameVariables.keyCount > 1)
             {
+                AudioSource.PlayClipAtPoint(clip, collider.transform.position, 2f);
                 GameVariables.keyCount = 0;
                 Destroy(gameObject);
             }
--- Assets/door/NormalGate.cs
+++ Assets/door/NormalGate.cs
@@ -4,11 +4,12 @@ namespace door
 {
     public class NormalGate : MonoBehaviour
     {
-        
+        [SerializeField] private AudioClip clip;
         private void OnTriggerEnter(Collider collider)
         {
             if (collider.CompareTag("Player"))
             {
+                AudioSource.PlayClipAtPoint(clip, collider.transform.position, 2f);
                 Destroy(gameObject);
             }
         }
